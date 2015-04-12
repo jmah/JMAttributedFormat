8d2676a22022c49bb1f2a93d6c6712016384d8da
Jonathon Mah <me@JonathonMah.com>
2015-04-12 13:34:44 -0700

    Use NS_ASSUME_NONNULL
    Without this, in certain configurations va_list would be a pointer and
    result in a complaint that its nullability wasn't specified (but in
    other configurations it wouldn't be a pointer and so it's illegal to
    specify nullability).

--- JMAttributedFormat.h
+++ JMAttributedFormat.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 
 
+NS_ASSUME_NONNULL_BEGIN
+
+
 @interface NSAttributedString (JMAttributedFormat)
 
 #pragma mark Formatting
@@ -17,28 +20,28 @@
  @param format The format string. Only \p %@ and \p %n$@ (e.g. \p %1$@ ) specifiers are supported
  @param ... A comma-separated list of arguments to substitute into \p format .
  */
-+ (nullable instancetype)attributedStringWithFormat:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
++ (nullable instancetype)attributedStringWithFormat:(NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
  @param format The format string. Only \p %@ and \p %n$@ (e.g. \p %1$@ ) specifiers are supported
  @param ... A comma-separated list of arguments to substitute into \p format .
  */
-+ (nullable instancetype)attributedStringWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
++ (nullable instancetype)attributedStringWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
  @param format The format string. Only \p %@ and \p %n$@ (e.g. \p %1$@ ) specifiers are supported
  @param ... A comma-separated list of arguments to substitute into \p format .
  */
-- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
+- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
  @param format The format string. Only \p %@ and \p %n$@ (e.g. \p %1$@ ) specifiers are supported
  @param argList A list of arguments to substitute into \p format .
  */
-- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format arguments:(va_list)argList NS_FORMAT_FUNCTION(2,0);
+- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format arguments:(va_list)argList NS_FORMAT_FUNCTION(2,0);
 
 
 #pragma mark Localized Formatting
@@ -47,7 +50,7 @@
  @param format The format string. Only \p %@ and \p %n$@ (e.g. \p %1$@ ) specifiers are supported
  @param ... A comma-separated list of arguments to substitute into \p format .
  */
-+ (nullable instancetype)localizedStringWithFormat:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
++ (nullable instancetype)localizedStringWithFormat:(NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted according to the current locale.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
@@ -55,7 +58,7 @@
  @param ... A comma-separated list of arguments to substitute into \p format .
  @discussion When arguments are substituted, \p -attributedDescriptionWithLocale: (or \p -descriptionWithLocale: ) is called instead of \p -attributedDescription (or \p -description ).
  */
-+ (nullable instancetype)localizedStringWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
++ (nullable instancetype)localizedStringWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format, ... NS_FORMAT_FUNCTION(2,3);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
@@ -63,7 +66,7 @@
  @param locale An \p NSLocale object specifying the locale to use when substituting argument values. To use the current locale, pass \p [NSLocale currentLocale]. To use the system locale, pass nil.
  @param ... A comma-separated list of arguments to substitute into \p format .
  */
-- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format locale:(nullable NSLocale *)locale, ... NS_FORMAT_FUNCTION(2,4);
+- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format locale:(nullable NSLocale *)locale, ... NS_FORMAT_FUNCTION(2,4);
 
 /** Returns an attributed string created by using the given format string as a template into which the remaining argument values are substituted.
  @param baseAttributes Attributes to apply to the literal parts of \p format , as well as argument values without an attributed description (e.g. plain \p NSString values)
@@ -71,7 +74,7 @@
  @param locale An \p NSLocale object specifying the locale to use when substituting argument values. To use the current locale, pass \p [NSLocale currentLocale]. To use the system locale, pass nil.
  @param argList A list of arguments to substitute into \p format .
  */
-- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(nonnull NSString *)format locale:(nullable NSLocale *)locale arguments:(va_list)argList NS_FORMAT_FUNCTION(2,0);
+- (nullable instancetype)initWithBaseAttributes:(nullable NSDictionary *)baseAttributes format:(NSString *)format locale:(nullable NSLocale *)locale arguments:(va_list)argList NS_FORMAT_FUNCTION(2,0);
 
 @end
 
@@ -88,3 +91,6 @@
 - (nullable NSAttributedString *)attributedDescriptionWithLocale:(nullable NSLocale *)locale;
 
 @end
+
+
+NS_ASSUME_NONNULL_END
